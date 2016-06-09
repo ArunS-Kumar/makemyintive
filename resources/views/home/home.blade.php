d1491f2ae8a65ae3fb56571e31f9379b1b094b7e
Arun Kumar <s.arunkumar@Arun-MacBook-Air.local>
2016-06-09 18:04:27 +0530

    Home page creation

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Make my Intivies </title>
+</head>
+<body>
+	 <div class="container">
+            <div class="content">
+                <div class="title">Welcome to Make my Intivies </div>
+            </div>
+        </div>
+</body>
+</html>
