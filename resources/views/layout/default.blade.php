9bcc22c7572d86b7edf68127069311b0daa5b60e
Arun Kumar <s.arunkumar@Arun-MacBook-Air.local>
2016-06-10 19:07:07 +0530

    Create basic layout

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Make My Invite</title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        @include('layout.partials.css')
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+            @include('layout.partials.topmenu')
+            @include('layout.partials.sidemenu')
+            @yield('content')
+            @include('layout.partials.footer')
+        </div>
+        <!-- ./wrapper -->
+        @include('layout.partials.js')
+    </body>
+</html>
---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+<div class="pull-right hidden-xs">
+</div>
+<strong>Copyright &copy; <a href="http://makemyinvite.com">Make My Invite</a>.</strong> 
+</footer>
+
+<div class="control-sidebar-bg"></div>
