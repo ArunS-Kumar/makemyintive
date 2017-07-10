8d792d85a3740c26e6514510e8164e5a2eeb0205
Arun Kumar <arunsak91@gmail.com>
2017-07-10 18:34:03 +0530

    created the basic structure of invite edit page

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,52 +1,221 @@
 @extends('layout.default')
 
 @section('content')
-
+<link rel="stylesheet" type="text/css" href="https://www.greetingsisland.com/Editor/css/editor.min.css?v=wCWjzEuCVpZUlTaj1o1aeC2dbErtI_LsmfgHjuRAqpc1">
  <!-- Full Width Column -->
   <div class="content-wrapper">
     <div class="container">
       <!-- Content Header (Page header) -->
       <section class="content-header">
         <h1>
-          Top Navigation
-          <small>Example 2.0</small>
+          Customize
+          <!-- <small>Example 2.0</small> -->
         </h1>
         <ol class="breadcrumb">
           <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-          <li><a href="#">Layout</a></li>
-          <li class="active">Top Navigation</li>
+          <li><a href="#">List</a></li>
+          <li class="active">Customize</li>
         </ol>
       </section>
 
       <!-- Main content -->
       <section class="content">
-        <div class="callout callout-info">
-          <h4>Tip!</h4>
+        
+                 
+            <div class=" editor-area item-invitation -portrait editor-section" id="edit-area">
 
-          <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a
-            sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular
-            links instead.</p>
-        </div>
-        <div class="callout callout-danger">
-          <h4>Warning!</h4>
 
-          <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar
-            and the content will slightly differ than that of the normal layout.</p>
-        </div>
-        <div class="box box-default">
-          <div class="box-header with-border">
-            <h3 class="box-title">Blank Box</h3>
-          </div>
-          <div class="box-body">
-            The great content goes here
-          </div>
-          <!-- /.box-body -->
-        </div>
+               <div class="box1 editorTab tabFront active" tab="front" style="float: left;">
+                  <div class="editorFrameContainer card-shadow">
+                     <div class="editorFrame ui-droppable" entity_id="11210">
+                        <div class="card_decor decor_full_image"><img src="/inviteImgs/Backgrounds/Floral-Swirls_8.png"></div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17445" style="left: 141.778px; top: 87.9787px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -7.5px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: merriweatherbold_italic; font-size: 15px; line-height: 18px; width: 163px; height: 18px;">You are invited to the</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17446" style="left: 134.167px; top: 115.515px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -24px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: great_vibesregular; font-size: 48px; line-height: 67.2px; width: 203px; height: 67.2px;">Wedding</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17447" style="left: 148.167px; top: 192.543px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -8px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: merriweatherbold_italic; font-size: 16px; line-height: 19.2px; width: 153px; height: 19.2px;">joining the lives of</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17448" style="left: 86.7778px; top: 235.997px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -17px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: great_vibesregular; font-size: 34px; line-height: 47.6px; width: 295px; height: 47.6px;">Miss Autumn Mercer</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17449" style="left: 142.778px; top: 393.998px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -8px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: merriweatherbold_italic; font-size: 16px; line-height: 19.2px; width: 163px; height: 114px;">Saturday. May 24th
+                                                                                                    at 2:00p.m.
+
+                                                                                                    Grace Church
+                                                                                                    432 Thurston Ave.
+                                                                                                    Greentown
+                                            </textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17450" style="left: 211.778px; top: 289.998px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -11.5px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: merriweatherbold_italic; font-size: 23px; line-height: 27.6px; width: 42px; height: 27.6px;">&</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="17451" style="left: 87.7778px; top: 320px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -16.5px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: great_vibesregular; font-size: 33px; line-height: 46.2px; width: 295px; height: 46.2px;">Mr. Alexander Shiloh</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor slot slot_text freesize dark-text-bg" entity_id="20586" style="left: 156.778px; top: 547.997px; outline-color: rgba(255, 255, 255, 0.95); margin-left: -6.5px;">
+                           <textarea class="textarea_style" placeholder=" " style="text-align: center; color: rgb(255, 255, 255); font-family: merriweatherbold_italic; font-size: 13px; line-height: 15.6px; width: 129px; height: 15.6px;">Reception to follow</textarea><a class="delete_handle" sticky_handle="sw" style="left: -15px; bottom: -15px;"></a>
+                        </div>
+                        <div class="card_decor decor_watermark" style="display:none"><img src="/Editor/images/watermark_invi.png"></div>
+                     </div>
+                  </div>
+                 
+               </div>
+
+
+            <div class="box2"> 
+
+                <div class="controlbox">
+                   <div class="controlheading"> <h1> Text Controls </h1> </div>
+                   <ul>
+                      
+                      <li>
+                         <p>Font color:</p>
+                         &nbsp;&nbsp; <input id="font-color" class="color{pickerClosable:true,hash:true}" onchange="fontcolor('#'+this.color)" value="000000" style="width: 60px; background-image: none; background-color: rgb(0, 0, 0); color: rgb(255, 255, 255);" autocomplete="off">
+                      </li>
+                      <li>
+                         <label for="font-family" class="col">Font Family: </label>
+                         <select id="font-family" class="btn btn-small" ="">
+                            <option>American Purpose</option>
+                            <option>Arial</option>
+                            <option>Black Ops One</option>
+                            <option>Chewy</option>
+                            <option>Courgette</option>
+                            <option>Courier</option>
+                            <option>FascinateInline</option>
+                            <option>Flavors</option>
+                            <option>Georgia</option>
+                            <option>Lemon</option>
+                            <option>LondrinaShadow</option>
+                            <option>LuckiestGuy</option>
+                            <option>Merienda</option>
+                            <option>ReenieBeanie</option>
+                            <option>Simonetta</option>
+                            <option>SpicyRice</option>
+                            <option>Tangerine</option>
+                         </select>
+                         <span class="caret"></span>   
+                      </li>
+                      <li>
+                         <label for="text-align" style="display:inline-block">Text align:</label>
+                         <select id="text-align" class="btn btn-small" ="">
+                            <option>Left</option>
+                            <option>Center</option>
+                            <option>Right</option>
+                            <option>Justify</option>
+                         </select>
+                         <span class="caret"></span>
+                      </li>
+                      <li>
+                         <p>Text Style:</p>
+                         <div class="btn-group" data-toggle="buttons-checkbox">
+                            <button type="button" class="btn btn-primary" id="bold-font" =""><b>Bold</b></button>
+                            <button type="button" class="btn btn-primary" id="italic-font" =""><i>Italic</i></button>
+                            <button type="button" class="btn btn-primary" id="text-shadow" ="">Shadow</button>
+                         </div>
+                      </li>
+                      
+                      
+                      <li>
+                         <label>Font width:</label> <input type="range" value="0" step="1" min="0" max="2" id="stroke-width" ="">
+                      </li>
+                    
+                   </ul>
+                </div>
+                </div>
+
+                <div class="box3"> <h1>Box 3</h1>  </div>
+                <div class="box4"> <h1>Box 4</h1>  </div>
+
+            </div>
+           
+   
+
+
         <!-- /.box -->
       </section>
       <!-- /.content -->
     </div>
     <!-- /.container -->
   </div>
+  <style type="text/css">
+        .editorFrame {
+            width: 450px;
+            height: 650px;
+        }
+        .box1, .box2, .box3, .box4 {
+            float: left;
+            margin-right: 20px; 
+            margin-bottom: 20px;
+        }
+        .controlbox {
+    display: inline-block;
+    vertical-align: top;
+    background-color: #fff;
+    width: 260px;
+    margin: 5px;
+    padding: 10px;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+.controlbox .controlheading {
+    background: none repeat scroll 0 0 #1A7BB7;
+    border-top: 1px solid #4BA3FF;
+    color: #fff;
+    font-weight: 700;
+    padding: 8px 5px;
+    text-align: center;
+    margin: -11px -11px 5px -11px;
+    font-size: 16px;
+}
+.controlbox ul {
+    margin: 0;
+    padding: 0;
+}
+.controlbox ul li {
+    display: inline-block;
+    margin: 5px 0;
+    padding: 2px 4px;
+}
+.controlbox label, .controlbox p {
+    color: #D39341;
+    display: inline-block;
+    margin: 0 5px 5px 0px;
+}
+select.btn {
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+    padding-right: 16px;
+    font-size: 14px;
+    width: 130px;
+}
+.btn-small {
+    padding: 2px 10px;
+    font-size: 11.9px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+}
+select.btn-small + .caret {
+    margin-left: -20px;
+    vertical-align: middle;
+}
+.caret {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    vertical-align: top;
+    border-top: 4px solid #000000;
+    border-right: 4px solid transparent;
+    border-left: 4px solid transparent;
+    content: "";
+}
+
+  </style>
 
 @stop
