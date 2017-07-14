82180ca351196e4b8c1315ecf6bee649d1d0e11b
Arun Kumar <arunsak91@gmail.com>
2017-07-14 13:49:05 +0530

    added all fonts and resize the main invite image layout

--- resources/views/layout/partials/css.blade.php
+++ resources/views/layout/partials/css.blade.php
@@ -1,5 +1,6 @@
 <!-- Bootstrap 3.3.6 -->
   <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts.css') }}" >
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
